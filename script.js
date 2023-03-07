b2a6584921581e6c8a1cefa5e38267ee6a7939b5
sofiazainesGitHub <125313264+sofiazainesGitHub@users.noreply.github.com>
2023-03-07 14:18:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const menuLinks = document.querySelectorAll(".menu a [href^="#"]");
+
+menuLinks.forEach(menuLink =>{  
+menuLinks.addEventListener("click", function())
+    menu.classList. remove("menu_opened");
+})
+
